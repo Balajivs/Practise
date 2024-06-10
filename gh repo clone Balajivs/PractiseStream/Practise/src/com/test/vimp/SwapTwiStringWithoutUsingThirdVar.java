96c8ea2525740f84d9c6104ded73a8d78cf5ca4d
Balajivs <balaji.sambhale33@gmail.com>
2024-06-10 22:06:57 +0530

    first commit

--- gh repo clone Balajivs/PractiseStream/Practise/src/com/test/vimp/SwapTwiStringWithoutUsingThirdVar.java
+++ gh repo clone Balajivs/PractiseStream/Practise/src/com/test/vimp/SwapTwiStringWithoutUsingThirdVar.java
@@ -9,7 +9,7 @@ public class SwapTwiStringWithoutUsingThirdVar {
 
 		// Concatenate both the string str1 and str2 and store it in str1
 		str1 = str1 + str2;
-		
+		System.out.println("-----------"+str1);
 		// Extract str2 from updated str1
 		str2 = str1.substring(0, (str1.length() - str2.length()));
 		
